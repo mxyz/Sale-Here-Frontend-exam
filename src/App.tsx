4c7772cb0a5297fbfccde9d02dcb6b598182cbc3
Kittipat <iam_max21@hotmail.com>
2024-01-26 04:30:42 +0700

    update lock

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import React from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 import Home from './pages/home'
+import images from './assets/images';
 
 const App = () => {
   return (
+
+    <main className="w-full h-full" style={{
+      backgroundImage: `url(${images.bg})`
+    }}>
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<div>404</div>} />
       </Routes>
     </BrowserRouter>
+    </main>
   )
 };
 
